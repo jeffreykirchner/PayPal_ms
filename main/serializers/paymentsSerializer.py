b814ab2bb3f806d0e2ff0c393217686285f4f7f0
Jeffrey Kirchner <jkirchner@gmail.com>
2021-02-01 21:39:14 -0800

    development

--- main/serializers/paymentsSerializer.py
+++ main/serializers/paymentsSerializer.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from main.models import Payments
 
-class PayementsSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class PayementsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = ['id', 'email', 'amount', 'memo']
+
+# class PayementsSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
     
-    email = serializers.EmailField(max_length = 250)
-    amount = serializers.DecimalField(min=0)
-    memo = serializers.CharField(max_length = 250) 
+#     email = serializers.EmailField(max_length = 250)
+#     amount = serializers.DecimalField(min=0)
+#     memo = serializers.CharField(max_length = 250) 
     
 
-    def create(self, validated_data):
-        """
-        Create and return a new 'Payements' instance, given the validated data.
-        """
-        return Payments.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         """
+#         Create and return a new 'Payements' instance, given the validated data.
+#         """
+#         return Payments.objects.create(**validated_data)
 
