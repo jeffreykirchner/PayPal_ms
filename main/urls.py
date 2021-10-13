ea826d76c462314f043c5d19b72bada0f9ab6b30
Jeffrey Kirchner <jkirchner@gmail.com>
2021-10-13 10:42:18 -0700

    Add time zone source to payment range view. Fixes #13.

--- main/urls.py
+++ main/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', views.root_path),
 
     path('payments/', views.PaymentListView.as_view()),
-    path('payments/<start_date>/<end_date>/<path:source_time_zone>', views.PaymentRangeView.as_view()),
+    path('payments/<str:start_date>/<str:end_date>/<path:source_time_zone>', views.PaymentRangeView.as_view()),
 
     #txt
     path('robots.txt', views.RobotsTxt, name='robotsTxt'),
